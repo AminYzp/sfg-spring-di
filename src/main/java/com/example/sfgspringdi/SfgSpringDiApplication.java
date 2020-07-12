2327d8947267789083bcdd7396714be621bbd2ab
AminYzp <64411984+AminYzp@users.noreply.github.com>
2020-07-12 21:15:41 +0430

    initial commit

--- src/main/java/com/example/sfgspringdi/SfgSpringDiApplication.java
+++ src/main/java/com/example/sfgspringdi/SfgSpringDiApplication.java
@@ -1,13 +1,18 @@
 package com.example.sfgspringdi;
 
+import com.example.sfgspringdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgSpringDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgSpringDiApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SfgSpringDiApplication.class, args);
+        MyController myController = (MyController) applicationContext.getBean("myController");
+        // Like the way is used in old Spring version for creating object by means of .getBean() method.
+        // note : beans name is same as the class name but the beginning with lower case. MyController => myController
+        System.out.println(myController.sayHello());
     }
-
 }
